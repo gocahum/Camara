709033255e6d82305fb88c9686265ff03a5b6d0d
gocahum <gocahum@gmail.com>
2018-09-06 18:52:05 -0500

    Prom, faltan dependencias

---
+++ src/main/java/CamaraTest.java
@@ -0,0 +1,19 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Humberto
+ */
+public class CamaraTest {
+    public static void main(String args []){
+    CamaraFrame camara = new CamaraFrame();
+    camara.setVisible(true);
+    }
+}
